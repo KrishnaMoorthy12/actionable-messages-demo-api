20459f268dbc082ea2a952466d2247501825f002
krishh <krishna.moorthy@zohocorp.com>
2022-11-02 15:15:27 +0530

    added verbose logging to display running port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Application } from './platform';
 
 async function bootstrap() {
   const server = await NestFactory.create(AppModule);
+  const logger = new Logger();
+  server.useLogger(logger);
   const app = await Application.getInstance();
-  await server.listen(app.getRunningPort());
+  const port = app.getRunningPort();
+  await server.listen(port, () => {
+    logger.verbose(`🚀 Server started on port ${port}`, 'ApplicationRoot');
+  });
 }
 bootstrap();
